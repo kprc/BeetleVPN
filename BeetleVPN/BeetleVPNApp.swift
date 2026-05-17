353d8e11e73b6debb7ff75e87a64d12b286babaf
kprc <micliao@gmail.com>
2026-05-17 21:27:23 +0800

    Initial Commit

---
+++ BeetleVPN/BeetleVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeetleVPNApp.swift
+//  BeetleVPN
+//
+//  Created by rickey on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BeetleVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeetleVPN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeetleVPN
+//
+//  Created by rickey on 5/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
